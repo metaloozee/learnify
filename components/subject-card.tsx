9e5993ea48cb9ada522a4a46704f68ce13061e0f
Ayan <ayanparkar123@gmail.com>
2024-01-08 12:18:07 +0530

    feat(under dev): student subject dashboard

--- components/subject-card.tsx
+++ components/subject-card.tsx
@@ -99,7 +99,7 @@ export const StudentSubjectCard = async ({
     return subjectData ? (
         <Link
             className="hover:-translate-y-1 transition-all duration-200"
-            href={`/subject/${subjectData.subjectid}`}
+            href={`/student/subject/${subjectData.subjectid}`}
         >
             <Card className="h-full w-full">
                 <CardHeader className="text-sm flex flex-row items-center justify-between space-y-0 pb-0">
