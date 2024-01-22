fdcd73ee9f94f188117b9b252b065d51dd369d6c
Ayan <ayanparkar123@gmail.com>
2024-01-22 12:47:48 +0530

    feat: refactored the code to re-use the component for both the users

--- components/note-card.tsx
+++ components/note-card.tsx
@@ -24,50 +24,74 @@ const getNote = async ({
     q,
     subjectid,
     limit = 10,
+    type,
 }: {
     q?: string | null
     subjectid: string
     limit?: number
+    type: "student" | "teacher"
 }) => {
     const supabase = await createServerSupabaseClient()
     const {
         data: { session },
     } = await supabase.auth.getSession()
 
-    if (q) {
+    if (type === "teacher") {
+        if (q) {
+            return await supabase
+                .from("notes")
+                .select("*")
+                .eq("teacherid", session?.user.id ?? "")
+                .eq("subjectid", subjectid)
+                .textSearch("notecontent", q, {
+                    type: "websearch",
+                })
+        }
+
         return await supabase
             .from("notes")
             .select("*")
             .eq("teacherid", session?.user.id ?? "")
             .eq("subjectid", subjectid)
-            .textSearch("notecontent", q, {
-                type: "websearch",
-            })
+            .limit(limit)
     }
 
-    return await supabase
-        .from("notes")
-        .select("*")
-        .eq("teacherid", session?.user.id ?? "")
-        .eq("subjectid", subjectid)
-        .limit(limit)
+    if (type === "student") {
+        if (q) {
+            return await supabase
+                .from("notes")
+                .select("*")
+                .eq("subjectid", subjectid)
+                .textSearch("notecontent", q, {
+                    type: "websearch",
+                })
+        }
+
+        return await supabase
+            .from("notes")
+            .select("*")
+            .eq("subjectid", subjectid)
+            .limit(limit)
+    }
 }
 
 export const NotesCard = async ({
     q,
     subjectid,
+    type,
 }: {
     q?: string
     subjectid: string
+    type: "student" | "teacher"
 }) => {
-    const notes = await getNote({ q: q, subjectid: subjectid })
+    const notes = await getNote({ q: q, subjectid: subjectid, type })
 
-    return notes.data && notes.data.length > 0 ? (
+    return notes && notes.data && notes.data.length > 0 ? (
         notes.data.map((note, index) => (
             <Link
                 key={index}
                 className="hover:-translate-y-1 transition-all duration-200"
-                href={`/teacher/notes/${note.noteid}`}
+                href={`/${type}/notes/${note.noteid}`}
             >
                 <Card className="h-full w-full">
                     <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
