d409f7a9c640492048a58102f6f02d6a43832467
Ayan <ayanparkar123@gmail.com>
2024-01-22 13:02:47 +0530

    feat(under dev): student notes dashboard

---
+++ app/student/notes/[id]/page.tsx
@@ -0,0 +1,50 @@
+import { createServerSupabaseClient } from "@/app/supabase-server"
+
+export default async function StudentNotesIndexPage({
+    params,
+}: {
+    params: any
+}) {
+    const supabase = await createServerSupabaseClient()
+
+    const {
+        data: { session },
+    } = await supabase.auth.getSession()
+    const { data: studentData } = await supabase
+        .from("users")
+        .select("*")
+        .eq("userid", session?.user.id ?? "")
+        .eq("usertype", "student")
+        .single()
+    const { data: noteData } = await supabase
+        .from("notes")
+        .select("*")
+        .eq("noteid", params.id)
+        .single()
+    const { data: subjectData } = await supabase
+        .from("studentenrollment")
+        .select("subjects(*)")
+        .eq("userid", studentData?.userid ?? "")
+        .eq("subjectid", noteData?.subjectid ?? "")
+        .single()
+
+    return session && studentData && noteData && subjectData ? (
+        <div className="mt-20 flex flex-col gap-5">
+            <h1 className="text-3xl md:text-4xl">
+                Exciting Things Are{" "}
+                <span className="text-muted-foreground">Coming Soon!</span>
+            </h1>
+            <p className="text-md text-muted-foreground">
+                Stay tuned for our upcoming launch. We can't wait to share it
+                with you.
+            </p>
+        </div>
+    ) : (
+        <div className="mt-20 flex flex-col gap-5">
+            <h1 className="text-3xl md:text-4xl">Unauthorized</h1>
+            <p className="text-md text-muted-foreground">
+                You don't have the necessary privileges to view this page.
+            </p>
+        </div>
+    )
+}
