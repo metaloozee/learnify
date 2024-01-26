c97c2cf55080f20547cc6510369f468770091829
Ayan <ayanparkar123@gmail.com>
2024-01-26 23:22:04 +0530

    feat: updated database schema

--- components/subject-card.tsx
+++ components/subject-card.tsx
@@ -1,16 +1,6 @@
 import Link from "next/link"
-import type { Session } from "@supabase/supabase-js"
 
-import { Badge } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button"
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
 import { Skeleton } from "@/components/ui/skeleton"
 import { createServerSupabaseClient } from "@/app/supabase-server"
 import type { Subject } from "@/app/teacher/page"
