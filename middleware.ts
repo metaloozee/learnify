4669d3e975310c54f83fb112a3150026ab7a8bc5
Ayan <ayanparkar123@gmail.com>
2024-02-08 12:35:36 +0530

    feat: rate limit

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,27 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server"
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs"
+import { Ratelimit } from "@upstash/ratelimit"
+import { Redis } from "@upstash/redis"
 
 import type { Database } from "@/types/supabase"
 
-export async function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest, event: NextFetchEvent) {
     const res = NextResponse.next()
     const supabase = createMiddlewareClient<Database>({ req, res })
+    const key = decodeURIComponent(req.nextUrl.pathname.split("/")[1])
+
+    const ip = req.ip
+
+    const ratelimit = new Ratelimit({
+        redis: Redis.fromEnv(),
+        limiter: Ratelimit.slidingWindow(5, "10 s"),
+    })
 
     const {
         data: { session },
     } = await supabase.auth.getSession()
 
-    if (session) {
+    if (session && key.length == 0) {
         const { data: userData, error: userError } = await supabase
             .from("users")
             .select("*")
@@ -29,6 +39,13 @@ export async function middleware(req: NextRequest) {
         if (userData.usertype === "teacher") {
             return NextResponse.redirect(new URL("/teacher", req.url))
         }
+    } else {
+        const { success, pending } = await ratelimit.limit(`mw_${ip}`)
+        event.waitUntil(pending)
+
+        if (!success) {
+            return NextResponse.redirect(new URL("/api/blocked", req.url))
+        }
     }
 
     return NextResponse.next()
@@ -43,6 +60,6 @@ export const config = {
          * - favicon.ico (favicon file)
          * Feel free to modify this pattern to include more paths.
          */
-        "/((?!_next/static|_next/image|favicon.ico|onboarding|student|teacher|subject|subjects|account|api).*)",
+        "/((?!_next/static|_next/image|favicon.ico).*)",
     ],
 }
