ccb6ec461f8d39c20de64622aee7f71b91983498
Ayan <ayanparkar123@gmail.com>
2024-02-08 12:37:33 +0530

    feat: updated env schema

--- env.mjs
+++ env.mjs
@@ -3,9 +3,10 @@ import { z } from "zod"
 const server = z.object({
     NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
-    HUGGINGFACE_API_KEY: z.string(),
     OPENAI_API_KEY: z.string(),
     RESEND_API_KEY: z.string(),
+    UPSTASH_REDIS_REST_URL: z.string().url(),
+    UPSTASH_REDIS_REST_TOKEN: z.string(),
     NODE_ENV: z.enum(["development", "test", "production"]),
 })
 
@@ -23,9 +24,10 @@ const client = z.object({
 const processEnv = {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    HUGGINGFACE_API_KEY: process.env.HUGGINGFACE_API_KEY,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     RESEND_API_KEY: process.env.RESEND_API_KEY,
+    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
     NODE_ENV: process.env.NODE_ENV,
 }
 
