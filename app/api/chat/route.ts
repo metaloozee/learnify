d6d15f22fd841d44b970f3431ae7a8712aee59d9
Ayan <ayanparkar123@gmail.com>
2024-02-17 21:10:45 +0530

    feat: document qna

---
+++ app/api/chat/route.ts
@@ -0,0 +1,70 @@
+import { env } from "@/env.mjs"
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai"
+import { Pinecone } from "@pinecone-database/pinecone"
+import { StreamingTextResponse, Message as VercelChatMessage } from "ai"
+import { PromptTemplate } from "langchain/prompts"
+import { BytesOutputParser } from "langchain/schema/output_parser"
+
+import { createServerSupabaseClient } from "@/app/supabase-server"
+
+export const runtime = "edge"
+
+const openai = new ChatOpenAI({})
+const pinecone = new Pinecone({
+    apiKey: env.PINECONE_API_KEY,
+})
+
+const formatMessage = (message: VercelChatMessage) => {
+    return `${message.role}: ${message.content}`
+}
+
+const TEMPLATE = `
+You are a Personal AI tutor and one of your student will be asking you questions or doubts.
+Given the following sections from the notes, answer the question using only that information.
+If you are unsure and the answer is not explicitly written in the notes, say "Sorry, I dont't know how to help with that."
+
+Context Sections:
+{contextSections}
+
+Question: {input}
+`
+
+export async function POST(req: Request) {
+    const supabase = await createServerSupabaseClient()
+    const {
+        data: { session },
+    } = await supabase.auth.getSession()
+    if (!session) {
+        return new Response("UNAUTHORIZED")
+    }
+
+    const body = await req.json()
+    const messages = body.messages ?? []
+    const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage)
+    const currentMessageContent = messages[messages.length - 1].content
+
+    const questionEmbedding = await new OpenAIEmbeddings().embedQuery(
+        currentMessageContent
+    )
+    const pineconeResults = await pinecone.Index("learnify").query({
+        vector: questionEmbedding,
+        topK: 5,
+        includeMetadata: true,
+        includeValues: false,
+    })
+
+    const contextSections = await pineconeResults.matches
+        .map((m) => m.metadata?.text)
+        .join()
+
+    const prompt = PromptTemplate.fromTemplate(TEMPLATE)
+    const outputParser = new BytesOutputParser()
+    const chain = prompt.pipe(openai).pipe(outputParser)
+
+    const stream = await chain.stream({
+        contextSections: contextSections,
+        input: currentMessageContent,
+    })
+
+    return new StreamingTextResponse(stream)
+}
--- components/ai-playground.tsx
+++ components/ai-playground.tsx
@@ -1,5 +1,11 @@
-import { Bot, SendHorizonal } from "lucide-react"
+"use client"
 
+import { useChat } from "ai/react"
+import { Bot, SendHorizonal, User } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { ScrollArea } from "@/components/ui/scroll-area"
 import {
     Sheet,
     SheetContent,
@@ -10,13 +16,13 @@ import {
     SheetTrigger,
 } from "@/components/ui/sheet"
 
-import { Button } from "./ui/button"
-import { Input } from "./ui/input"
-
 export type ChatPlaygroundProps = {
     id: string
 }
+
 export const AiChatPlayground = ({ id }: ChatPlaygroundProps) => {
+    const { messages, input, handleInputChange, handleSubmit } = useChat()
+
     return (
         <Sheet>
             <SheetTrigger asChild>
@@ -26,20 +32,44 @@ export const AiChatPlayground = ({ id }: ChatPlaygroundProps) => {
                 </Button>
             </SheetTrigger>
             <SheetContent className="w-full h-screen flex flex-col justify-between">
-                <SheetHeader>
+                <SheetHeader className="text-left">
                     <SheetTitle>
                         <Bot className="mr-2 h-5 w-5" />
                         Hey there, human friend! 👋
                     </SheetTitle>
-                    <SheetDescription className="text-xs">
+                    <SheetDescription className="text-xs pb-5">
                         I'm your virtual buddy here to make learning a breeze
                         and your doubts disappear faster than you can say
                         "supercalifragilisticexpialidocious"!
                     </SheetDescription>
+                    <ScrollArea className="h-[500px]">
+                        {messages.map((m) => (
+                            <div
+                                className="py-4 px-2 rounded bg-muted flex gap-2 items-start"
+                                key={m.id}
+                            >
+                                <div>
+                                    {m.role === "user" ? (
+                                        <User className="h-4 w-4" />
+                                    ) : (
+                                        <Bot className="h-4 w-4" />
+                                    )}
+                                </div>
+                                <div>{m.content}</div>
+                            </div>
+                        ))}
+                    </ScrollArea>
                 </SheetHeader>
                 <SheetFooter>
-                    <form className="w-full flex flex-row gap-2" action="">
-                        <Input placeholder="Explain computer to a 5 year old" />
+                    <form
+                        className="w-full flex flex-row gap-2"
+                        onSubmit={handleSubmit}
+                    >
+                        <Input
+                            value={input}
+                            onChange={handleInputChange}
+                            placeholder="Explain computer to a 5 year old"
+                        />
                         <Button type="submit">
                             <SendHorizonal className="w-4 h-4" />
                         </Button>
