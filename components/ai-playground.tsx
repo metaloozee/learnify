fae76e0721a8d86b07e0ed856c9c739fd1a7a5d6
Ayan <ayanparkar123@gmail.com>
2024-03-08 12:05:19 +0530

    fix: tutor

--- components/ai-playground.tsx
+++ components/ai-playground.tsx
@@ -26,34 +26,29 @@ export const AiChatPlayground = () => {
                     <Bot className="w-4 h-4" />
                 </Button>
             </SheetTrigger>
-            <SheetContent className="w-full overflow-auto flex flex-col justify-between">
-                <SheetHeader className="text-left">
+            <SheetContent className="container overflow-auto flex min-w-full">
+                <SheetHeader className="text-left fixed backdrop-blur-md w-full">
                     <SheetTitle>
                         <Bot className="mr-2 h-5 w-5" />
                         Hey there, human friend! 👋
                     </SheetTitle>
-                    <SheetDescription className="text-xs pb-5">
-                        I'm your virtual buddy here to make learning a breeze
-                        and your doubts disappear faster than you can say
-                        "supercalifragilisticexpialidocious"!
-                    </SheetDescription>
-                    <ScrollArea className="h-[500px]">
-                        {messages.map((m, index) =>
-                            m.role === "user" ? (
-                                <div className="py-4 px-2 rounded bg-muted">
-                                    <User className="h-4 w-4" />
-                                    {m.content}
-                                </div>
-                            ) : (
-                                <div className="my-2 py-4 px-2 rounded bg-green-100/50">
-                                    <Bot className="h-4 w-4" />
-                                    {m.content}
-                                </div>
-                            )
-                        )}
-                    </ScrollArea>
                 </SheetHeader>
-                <SheetFooter>
+                <ScrollArea className="h-auto mt-16 pb-16">
+                    {messages.map((m, index) =>
+                        m.role === "user" ? (
+                            <div className="py-4 px-2 rounded bg-muted">
+                                <User className="h-4 w-4" />
+                                {m.content}
+                            </div>
+                        ) : (
+                            <div className="my-2 py-4 px-2 rounded bg-green-100/50">
+                                <Bot className="h-4 w-4" />
+                                {m.content}
+                            </div>
+                        )
+                    )}
+                </ScrollArea>
+                <SheetFooter className="fixed bottom-5 pr-12 bg-white w-full">
                     <form
                         className="w-full flex flex-row gap-2"
                         onSubmit={handleSubmit}
