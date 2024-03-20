2bb7b44dc549c1fd7c9307615b156a9685db0bc6
Ayan <ayanparkar123@gmail.com>
2024-03-20 12:49:13 +0530

    fix: form

--- components/quiz.tsx
+++ components/quiz.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import React, { useState } from "react"
+import React from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { Check, GraduationCap, RotateCw, Send } from "lucide-react"
+import { Check, RotateCw, Send } from "lucide-react"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
 
@@ -44,6 +44,7 @@ export const Quiz = ({ quiz }: { quiz: QuizProps }) => {
         <form
             onSubmit={form.handleSubmit(async (data) => {
                 await evaluateQuizAnswer(data).then((value: any) => {
+                    form.reset()
                     return toast({
                         title: value.title,
                         description: value.description,
